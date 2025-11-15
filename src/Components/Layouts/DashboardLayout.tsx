13883470e2b17845e3e00a20854467206bcc4dc1
JefrinAkterJui <jefrin.jui.dev@gmail.com>
2025-11-15 20:50:54 +0600

    Ride analytics and request a ride done

--- src/Components/Layouts/DashboardLayout.tsx
+++ src/Components/Layouts/DashboardLayout.tsx
@@ -1,14 +1,47 @@
 import { Outlet } from "react-router";
-import Sidebar from "../Modules/Admin/Sidebar";
 import Navbar from "../Modules/Admin/Navbar";
+import { useUserInfoQuery } from "@/redux/freatures/auth/auth.api"; 
+import RiderSidebar from "../Modules/Rider/RiderSidebar"; 
+import { Skeleton } from "../ui/skeleton";
+import AdminSidebar from "../Modules/Admin/AdminSidebar";
 
 const DashboardLayout = () => {
+  const { data, isLoading } = useUserInfoQuery(undefined);
+  const userRole = data?.data?.role;
+
+  const renderSidebar = () => {
+    if (isLoading) {
+      return (
+        <div className="flex flex-col h-full justify-between p-6 border-r space-y-4">
+          <div className="space-y-3">
+            <Skeleton className="h-8 w-full" />
+            <Skeleton className="h-8 w-full" />
+            <Skeleton className="h-8 w-full" />
+            <Skeleton className="h-8 w-full" />
+          </div>
+          <Skeleton className="h-6 w-1/2" />
+        </div>
+      )
+    }
+
+    switch (userRole) {
+      case "ADMIN":
+        return <AdminSidebar />;
+      case "RIDER":
+        return <RiderSidebar />;
+      case "DRIVER":
+        // return <DriverSidebar />;
+      default:
+        return <div>Error: Unknown Role</div>; 
+    }
+  };
+
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <div className=" grid grid-cols-12 ">
         <div className=" h-[calc(100vh-50px)] col-span-2 ">
-          <Sidebar />
+          {renderSidebar()}
         </div>
         <div className="col-span-10 h-[calc(100vh-50px)] overflow-y-auto">
           <Outlet />
